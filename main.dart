cca116d837e9bd66c8c39b2dc8288dd82b38b7b1
Gospodi <gospodijazzet@gmail.com>
2022-06-04 16:52:02 +0300

    проверка возраста на условие

--- main.dart
+++ main.dart
@@ -1,3 +1,18 @@
+void main(List<String> args) {
+  const myAge = 40;
+  const isVotingAge = myAge >= 18;
+
+  print(isVotingAge);
+}
+
+
+
+
+
+
+
+
+
 // void main(List<String> args) {
 //   var welcomeMessage = 'hello world';
 //   welcomeMessage += ', it is good to see you';
@@ -13,28 +28,31 @@
 //   print(welcomeMessage);
 // }
 
-void main(List<String> args) {
-  const passingGrade = 50;
-  // const studentGrade = 74;
-  const studentGrade = 50;
+// void main(List<String> args) {
+//   const passingGrade = 50;
+//   // const studentGrade = 74;
+//   const studentGrade = 50;
 
-  // const studentPassed = studentGrade > passingGrade;
-  const studentPassed = studentGrade >= passingGrade;
-  print(studentPassed);
+//   // const studentPassed = studentGrade > passingGrade;
+//   const studentPassed = studentGrade >= passingGrade;
+//   print(studentPassed);
 
-  const studentFailed = studentGrade < passingGrade;
-  print(studentFailed);
+//   const studentFailed = studentGrade < passingGrade;
+//   print(studentFailed);
 
-  const chrisGrade = 49;
-  const samGrade = 99;
+//   const chrisGrade = 49;
+//   const samGrade = 99;
 
-  print(samGrade != chrisGrade);
+//   print(samGrade != chrisGrade);
 
-  const nameCat = 'Буся';
-  const nameDog = 'Вася';
+//   const nameCat = 'Буся';
+//   const nameDog = 'Вася';
 
-  print(nameCat != nameDog);
+//   print(nameCat != nameDog);
 
-  // if studentGrade
-  // print(studentGrade);
-}
+//   // if studentGrade
+//   // print(studentGrade);
+// }
+
+
+
