04bb37f65240ecf11c2aa469987565373129b353
Gospodi <gospodijazzet@gmail.com>
2022-06-04 16:26:55 +0300

    добавляем булевые переменные

--- main.dart
+++ main.dart
@@ -1,10 +1,21 @@
+// void main(List<String> args) {
+//   var welcomeMessage = 'hello world';
+//   welcomeMessage += ', it is good to see you';
+//   const boillingPoint = 100;
+//   var gradePointAverage = 3.5;
+
+//   // Добавление коментария
+
+//   /*
+//   многострочный коментарий
+//   */
+
+//   print(welcomeMessage);
+// }
+
 void main(List<String> args) {
-  var welcomeMessage = 'hello world';
-  welcomeMessage += ', it is good to see you';
-  const boillingPoint = 100;
-  var gradePointAverage = 3.5;
+  const bool yes = true;
+  const bool no = false;
 
-  // Добавление коментария
-
-  print(welcomeMessage);
+  print(yes);
 }
