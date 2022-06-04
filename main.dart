10be533a3843d07b76eb45ac28e2c328bfd43bce
Gospodi <gospodijazzet@gmail.com>
2022-06-04 16:02:56 +0300

    добавлены переменные и константы

---
+++ main.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  var welcomeMessage = 'hello world';
+  welcomeMessage += ', it is good to see you';
+  const boillingPoint = 100;
+  var gradePointAverage = 3.5;
+
+  print(welcomeMessage);
+}
