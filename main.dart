7fe1b1bb7711987ac2fb19787b3211b2f231d6f9
Gospodi <gospodijazzet@gmail.com>
2022-06-07 18:30:39 +0300

    определяем переменные

--- main.dart
+++ main.dart
@@ -1,26 +1,34 @@
-// Нулевая безопасность
-
-void main(List<String> args) {
-  var grades = List<int?>.empty(growable: true);
-  grades.add(100);
-  grades.add(null);
-  grades.add(84);
-
-  var total = 0;
-  var firstTest = grades[0];
-
-  if (firstTest != null) {
-    total += firstTest;
-  }
-  total += grades[1] ?? 0;
-  total += grades[2]!;
-
-  print(grades);
-  print(total);
-
-  var average = total / grades.length;
-  print('The average is $average');
+void main() {
+  var johnTest = 84;
+  var tedTest = 32;
+  var suzyTest = 80;
 }
+
+
+
+// // Нулевая безопасность
+
+// void main(List<String> args) {
+//   var grades = List<int?>.empty(growable: true);
+//   grades.add(100);
+//   grades.add(null);
+//   grades.add(null);
+
+//   var total = 0;
+//   var firstTest = grades[0];
+
+//   if (firstTest != null) {
+//     total += firstTest;
+//   }
+//   total += grades[1] ?? 0;
+//   total += grades[2]!;
+
+//   print(grades);
+//   print(total);
+
+//   var average = total / grades.length;
+//   print('The average is $average');
+// }
 
 
 
